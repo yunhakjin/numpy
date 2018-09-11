af6be940ba59780025feca42dafa6c0e35fae16d
yunhakjin <yunhakjin@163.com>
2018-09-11 18:01:25 +0800

    Signed-off-by: yunhakjin <yunhakjin@163.com>

---
+++ numpy.py
@@ -0,0 +1,8 @@
+__author__ = 'yxz'
+import numpy as np
+
+print(np.array([1,2,3]))
+print(np.array([[1,2],[3,4]]))
+print(np.array([1,  2,  3,4,5]))
+print(np.array([1,  2,  3,4,5]),ndmin=2)
+print(np.array([1,2,3],dtype=complex))
