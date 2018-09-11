a60d9d11889a9e10fb9bb65ffd28bcc38cffaee4
yunhakjin <yunhakjin@163.com>
2018-09-11 18:08:43 +0800

    Signed-off-by: yunhakjin <yunhakjin@163.com>

--- python_numpy.py
+++ python_numpy.py
@@ -4,5 +4,4 @@ import numpy as np
 print(np.array([1,2,3]))
 print(np.array([[1,2],[3,4]]))
 print(np.array([1,  2,  3,4,5]))
-print(np.array([1,  2,  3,4,5]),ndmin=2)
 print(np.array([1,2,3],dtype=complex))
